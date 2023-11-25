d7dfa82e7c32a0b3f8dd15ab1b2b6880f3d75546
6a6ak <babak.ahari@gmail.com>
2023-11-25 05:09:32 +0100

    modal

--- Chrome_Extension/popup.js
+++ Chrome_Extension/popup.js
@@ -225,3 +225,33 @@ taskDiv.appendChild(taskText);
         listDiv.appendChild(taskDiv);
     });
 }
+
+
+/*modal*/
+document.getElementById('openModalButton').addEventListener('click', function() {
+    document.getElementById('taskModal').style.display = 'block';
+});
+
+document.getElementById('closeModalButton').addEventListener('click', function() {
+    document.getElementById('taskModal').style.display = 'none';
+});
+
+document.getElementById('addTaskButton').addEventListener('click', function() {
+    var taskText = document.getElementById('taskText').value;
+    var taskSchedule = document.getElementById('taskSchedule').value;
+    var taskDelay = document.getElementById('taskDelay').value;
+    
+   /* var taskDiv = document.createElement('div');
+    taskDiv.innerHTML = `<strong>Task:</strong> ${taskText}<br>
+                         <strong>Schedule:</strong> ${taskSchedule}<br>
+                         <strong>Delay:</strong> ${taskDelay} seconds`;*/
+    document.getElementById('taskContainer').appendChild(taskDiv);
+
+    // Clear Form Fields
+    document.getElementById('taskText').value = '';
+    document.getElementById('taskSchedule').value = '';
+    document.getElementById('taskDelay').value = '';
+
+    // Close Modal
+    document.getElementById('taskModal').style.display = 'none';
+});
